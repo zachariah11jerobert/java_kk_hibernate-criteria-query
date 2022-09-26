629bb9ec20743fa77c6ac492e247daca7c16c311
zachariah11jerobert <zachariah11jerobert@gmail.com>
2022-09-26 12:20:36 +0530

    Hibernate-Query-Select-Enity

---
+++ src/main/java/com/jerome/client/CriteriaQueryEntitySelectionClientTest.java
@@ -0,0 +1,38 @@
+package com.jerome.client;
+
+import java.util.List;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.query.Query;
+
+import com.jerome.entities.Employee;
+import com.jerome.util.HibernateUtil;
+
+
+public class CriteriaQueryEntitySelectionClientTest {
+
+	public static void main(String[] args) {
+
+		try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+			CriteriaBuilder builder = session.getCriteriaBuilder();
+			CriteriaQuery<Employee> criteriaQuery = builder.createQuery(Employee.class);
+			Root<Employee> root = criteriaQuery.from(Employee.class);
+			criteriaQuery.select(root);
+			
+			criteriaQuery.where(builder.equal(root.get("employeeId"), 2));
+			
+			Query<Employee> query = session.createQuery(criteriaQuery);
+			List<Employee> empList = query.list();
+			empList.forEach(System.out::println);
+		} catch (HibernateException e) {
+			e.printStackTrace();
+		}
+	
+	}
+
+}
---
+++ src/main/java/com/jerome/client/SaveDataClientTest.java
@@ -0,0 +1,39 @@
+package com.jerome.client;
+
+import java.util.Date;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+
+import com.jerome.entities.Employee;
+import com.jerome.util.HibernateUtil;
+
+
+
+public class SaveDataClientTest {
+
+	public static void main(String[] args) {
+
+		try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+			session.beginTransaction();
+			Employee employee1 = new Employee();
+			employee1.setEmployeeName("Martin Bingel");
+			employee1.setEmail("martin.cs2017@gmail.com");
+			employee1.setSalary(50000.00);
+			employee1.setDoj(new Date());
+
+			Employee employee2 = new Employee();
+			employee2.setEmployeeName("Sean Murphy");
+			employee2.setEmail("sean.m2017@gmail.com");
+			employee2.setSalary(90000.00);
+			employee2.setDoj(new Date());
+
+			session.save(employee1);
+			session.save(employee2);
+
+			session.getTransaction().commit();
+		} catch (HibernateException e) {
+			e.printStackTrace();
+		}
+	}
+}
